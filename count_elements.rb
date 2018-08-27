976e1f8b1e260630fbd681a3b53c74f3f7aa386c
patti-c <patricia.carter@flatironschool.com>
2018-08-27 18:31:51 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,23 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+
+  # Go over each element in the array
+
+  array.each do |element|
+
+    # If the key already exists, we just want to
+    # increase it.
+
+    if new_hash.has_key?(element) == true
+      new_hash[element] += 1
+
+    # Otherwise we will create a new element
+
+    else
+      new_hash[element] = 1
+    end
+  end
+  return new_hash
 end
- 
